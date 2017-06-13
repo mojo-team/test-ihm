5820f63dc58e67f9801aaefd25f8f9ecf581b67c
Fabien Aubert <fabien.aubert@pole-emploi.fr>
2017-06-13 16:18:08 +0200

    chore(firebase): premiere integration degueulasse firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+
+import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
+import {AngularFireAuth} from "angularfire2/auth";
+import {Observable} from "rxjs/Observable";
+import * as firebase from "firebase/app";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+    this.user = this.afAuth.authState;
+  }
+
+  loginAnonyme() {
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  loginGoogle() {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  Send(desc: string) {
+    this.items.push({message: desc});
+    this.msgVal = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import { AlertModule } from 'ngx-bootstrap';
+import {AlertModule} from "ngx-bootstrap";
 
-import { AppComponent } from './app.component';
+import {AngularFireDatabaseModule} from "angularfire2/database";
+import {AngularFireAuthModule} from "angularfire2/auth";
+
+import {AppComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AngularFireModule} from "angularfire2";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCvjEn9dqAMZGbnCVa5lMBTVnLJzNbvdl8",
+  authDomain: "mojopeinc.firebaseapp.com",
+  databaseURL: "https://mojopeinc.firebaseio.com",
+  projectId: "mojopeinc",
+  storageBucket: "mojopeinc.appspot.com",
+  messagingSenderId: "445519055003"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, AlertModule.forRoot()
+    BrowserModule,
+    AlertModule.forRoot(),
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
