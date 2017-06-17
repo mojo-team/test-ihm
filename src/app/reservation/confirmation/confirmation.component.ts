ea47ac9a1276e7928eb6e5545a873688e1d72e57
Christiansen MEREL <christiansen.merel@gmail.com>
2017-06-17 05:53:59 +0200

    affichage confirmation et reservation facility moche

--- src/app/reservation/confirmation/confirmation.component.ts
+++ src/app/reservation/confirmation/confirmation.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Facility {
+  public name: String;
+  public price: Number;
+  public selected: boolean;
+}
+
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmationComponent implements OnInit {
 
+  public facilities: Array<Facility> = [
+    { name: 'coffee', price: 7.5, selected: false },
+    { name: 'visio', price: 100, selected: false },
+    { name: 'edredon', price: 1000, selected: false },
+    { name: 'hotesse', price: 500, selected: false }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public total() : Number {
+    return this.facilities.filter(facility => facility.selected).reduce((total, element) => total+element.price.valueOf(), 0);
+  }
 }
