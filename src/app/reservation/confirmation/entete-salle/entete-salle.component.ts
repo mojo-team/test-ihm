054d16a2bafdb3d5f5a581dc96564f418bca34ec
Roux Jérôme <roux.jerome@gmail.com>
2017-06-17 08:55:47 +0200

    entete OK

--- src/app/reservation/confirmation/entete-salle/entete-salle.component.ts
+++ src/app/reservation/confirmation/entete-salle/entete-salle.component.ts
@@ -1,12 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input} from '@angular/core';
 import {Salle} from "../../../service/salle";
+declare var $: any;
 
 @Component({
   selector: 'entete-salle',
   templateUrl: './entete-salle.component.html',
   styleUrls: ['./entete-salle.component.css']
 })
-export class EnteteSalleComponent implements OnInit {
+export class EnteteSalleComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    //$('.carousel.carousel-slider').carousel({fullWidth: true});
+  }
 
   @Input() public salle: Salle;
 
