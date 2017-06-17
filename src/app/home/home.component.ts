c97183936d094ad854e1cb64b260508660d57af0
christophesamson <christophe.samson@free.fr>
2017-06-17 04:29:23 +0200

    ajout de l'experience reunir

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,7 +20,8 @@ export class HomeComponent implements OnInit {
     { nom: 'zen', image: 'experiences/zen.jpg', route: 'reservation/resultat' },
     { nom: 'création', image: 'experiences/creation.jpg', route: 'reservation/resultat' },
     { nom: 'copains', image: 'experiences/copains.jpg', route: 'reservation/resultat/copains' },
-    { nom: 'concentration', image: 'experiences/concentration.jpg', route: 'reservation/resultat' }
+    { nom: 'concentration', image: 'experiences/concentration.jpg', route: 'reservation/resultat' },
+    { nom: 'reunion', image: 'experiences/reunir.jpg', route: 'reservation/resultat' }
   ];
 
 }
