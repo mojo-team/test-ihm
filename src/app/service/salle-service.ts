9ddcffdf0a51860933c39d6ff4f51e1d5a3e2e1f
Fabien Aubert <fabien.aubert@pole-emploi.fr>
2017-06-17 04:15:41 +0200

    chore(salle-service): definition des services pour les salles

--- src/app/service/salle-service.ts
+++ src/app/service/salle-service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Salle} from "./salle";
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/observable/of';
+import * as firebase from "firebase/app";
 
 
 const SALLES: Salle[] = [{experience: 'zen', lieu: 'Paris', capacite: 5}];
@@ -14,6 +15,14 @@ export class SalleService {
   }
 
   public recupereLesSalles(localisation: String, date: Date, participants: Number, duree: Number): Observable<Salle[]> {
+    return Observable.of<Salle[]>(SALLES);
+  }
+
+  public recupereLesSallesRecommandeesPour(utilisateur: firebase.User): Observable<Salle[]> {
+    return Observable.of<Salle[]>(SALLES);
+  }
+
+  public recupereLesSallesParExperience(experience: string): Observable<Salle[]> {
     return Observable.of<Salle[]>(SALLES);
   }
 }
