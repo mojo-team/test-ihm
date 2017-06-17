2815159c3741cd88d4121fb72c7f82c69a443a5d
Roux Jérôme <roux.jerome@gmail.com>
2017-06-17 05:43:32 +0200

    renomer plus début intégration facebook messenger

--- src/app/noyau/navbar/navbar.component.ts
+++ src/app/noyau/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AngularFireAuth} from "angularfire2/auth";
 declare var FB: any;
@@ -9,17 +9,15 @@ declare var FB: any;
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  ngOnInit(): void {
-
+export class NavbarComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
     FB.init({
       appId: "1294487654003054",
       xfbml: true,
       version: "v2.6"
     });
-
-
   }
+
 
   constructor(public afAuth: AngularFireAuth, public router: Router) {
   }
